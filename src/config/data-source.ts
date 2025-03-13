fba018d4509ebb31fa68052101c4afecac2fa4f8
GamesProSeif <seifmansour2014@outlook.com>
2025-03-13 22:44:48 +0200

    Basic server + env setup

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -14,11 +14,13 @@ import { User } from "../models/User";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
+  // url: process.env.DB_URL,
   host: process.env.DB_HOST || "localhost",
   port: parseInt(process.env.DB_PORT || "5432"),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: true,
   synchronize: true,
   logging: false,
   entities: [
